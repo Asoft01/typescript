8617d7453b0265e28535f121e825867ddbd66558
Adeleke Hammed <lekhad19@gmail.com>
2024-03-10 17:45:32 +0100

    implicit and explicit type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-let age: number =20; 
-console.log('Hello World');
+// let age: number =20; 
+// console.log('Hello World');
+
+// let sales: number = 123_456_789; 
+// let course: string = 'Typescript'; 
+// let is_published: boolean = true;
+// let level;
+
+let sales = 123_456_789; 
+let course = 'Typescript'; 
+let is_published = true;
+let level;
+// level = 1;
+// level = 'a';
+
+function render(document: any){
+    console.log(document);
+}
