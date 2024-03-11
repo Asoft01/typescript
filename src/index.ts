870331df00650dee705a102011bab61a9ad51df8
Adeleke Hammed <lekhad19@gmail.com>
2024-03-11 14:08:17 +0100

    enum

--- src/index.ts
+++ src/index.ts
@@ -29,4 +29,9 @@ let level;
 // 1, 'Mosh'
 let user: [number, string, boolean, number] = [1, 'Mosh', true, 0];
 
-// enums
+// const small = 1; 
+// const medium = 2; 
+// const large = 3; 
+
+// PascalCase
+// enum Size { Small = 1, Medium =2, Large=3 };
